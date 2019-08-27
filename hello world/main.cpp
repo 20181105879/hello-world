cf4a97cd2cbe79094d839722ef1036e62c0d530b
20181105879 <2605753145@qq.com>
2019-08-27 17:39:56 +0800

    paixu
    paixu

--- hello world/main.cpp
+++ hello world/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
-
+using namespace std;
+#define n 10
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int s[10]={1,2,3,4,6,7,9,8,0,5};
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-i-1;j<n-1;j++)
+        {
+            if(s[j+1]<s[j])
+            {
+                int t=s[j+1];
+                s[j+1]=s[j];
+                s[j]=t;
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+    
     return 0;
 }
